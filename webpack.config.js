e73a7928d6bc1a1210a8277bceecceb80dae0016
Candra Rahmawan <candra.rahmawan@rumah123.com>
2018-07-18 12:42:29 +0700

    Extract css into file

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  entry: './src/clients/Index.js',
+  entry: {
+    app: './src/clients/Index.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: '[name].[hash].js'
   },
   module: {
     rules: [
       {test: /\.js$/, use: 'babel-loader', exclude: /node_modules/},
-      {test: /\.scss$/, use: ['style-loader', 'css-loader']}
+      {test: /\.s?[c]ss$/, use: [MiniCssExtractPlugin.loader, 'css-loader']}
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'})
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new MiniCssExtractPlugin({
+      filename: "styles.[hash].css",
+    }),
+    new OptimizeCSSAssetsPlugin({})
   ],
   devServer: {
     port: 7000
